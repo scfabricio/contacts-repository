c93ca603e9223aa510f32a919ae4a38cf480f32b
Fabrício Sobreira Carneiro <scfabricio15@gmail.com>
2020-05-04 19:17:32 -0300

    Vesion 1.0 page web Demo.

---
+++ precache-manifest.28be5a53fc7034b5bf0bb12644b2908d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619297c35a00c2c891cc97e6a54fff5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c82cbeb785135540fb57",
+    "url": "/static/css/main.9f57ddec.chunk.css"
+  },
+  {
+    "revision": "9107cd2d1d44f298f2e0",
+    "url": "/static/js/2.e0dddf73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0dddf73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c82cbeb785135540fb57",
+    "url": "/static/js/main.880745a0.chunk.js"
+  },
+  {
+    "revision": "e805c25b4204dbe8397c",
+    "url": "/static/js/runtime-main.c1f08748.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/static/media/logo.82a7c062.svg"
+  }
+]);
